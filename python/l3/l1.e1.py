3211e5faa3a140cc320b578c0f12a08335e99d3d
citygym <143324870+citygym@users.noreply.github.com>
2024-04-12 21:02:17 +0300

    Add files via upload

---
+++ python/l3/l1.e1.py
@@ -0,0 +1,35 @@
+# Aceasta este prima ta sarcină legată de variabile în python
+# Creează o variabilă numită `age` și seteaz-o la vârsta ta
+age=16
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează valoarea variabilei `age` folosind funcția `print`
+print(age)
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum schimbă valoarea variabilei `age` la vârsta prietenului tău și afișează valoarea variabilei `age` folosind funcția `print`
+age=17
+print(age)
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum creează o nouă variabilă numită `age2` și seteaz-o la valoarea variabilei `age` și afișează valoarea variabilei `age2` folosind funcția `print`
+age2=age
+print(age2)
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum șterge variabila `age` și încearcă să afișezi valoarea variabilei `age` folosind funcția `print`
+del age
+print(age2)
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Asta a fost tot pentru această sarcină
---
+++ python/l3/l1e3.py
@@ -0,0 +1,89 @@
+# Aceasta este a treia ta sarcină legată de numere în python
+# Creează o variabilă numită `number` și atribuie-i valoarea `10`
+number=10
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează valoarea variabilei `number` folosind funcția `print`
+print(number)
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum creează o nouă variabilă numită `number2` și atribuie-i valoarea `number` și afișează valoarea variabilei `number2` folosind funcția `print`
+number2=number
+print(number2)
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum creează o nouă variabilă numită `number3` și atribuie-i valoarea `4`
+number3=4
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum aplică toate operațiile pe care le-ai învățat în lecție asupra variabilelor `number` și `number3` și afișează rezultatele folosind funcția `print`
+# Operațiile sunt: adunare, scădere, înmulțire, împărțire, modulo, exponențiere, împărțire întreagă
+print("ya")
+print(number + number3)
+print(number - number3)
+print(number * number3)
+print(number / number3)
+print(number % number3)
+print(number ** number3)  #!!!!
+print(number // number3)
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Creează o nouă variabilă numită `number4` și atribuie-i o valoare numerică mare într-un mod literal
+number4=7096
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează tipul variabilei `number4` folosind funcția `print`
+print(type(number4))
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Creați o variabilă `numar` și atribuiții valoarea 5.0
+numar=5.0
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișați tipul lui `numar` folosind funcția `print`
+print(type(numar))
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum creați o variabilă `numar2` și atribuiții valoarea 2.0
+numar2=2.0
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum creați o variabila `numar_mare` și atribuiți-i o valoare mare utilizând numerele literale
+numar_mare = 1+2j
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișați tipul lui `numar_mare` folosind funcția `print`
+print(type(numar_mare))
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Acum ștergeți variabila `numar_mare` folosind instrucțiunea `del`
+del numar_mare
+# CODUL TĂU VINE MAI JOS:
+
+# CODUL TĂU VINE MAI SUS:
+
+# Asta a fost tot pentru această sarcină
